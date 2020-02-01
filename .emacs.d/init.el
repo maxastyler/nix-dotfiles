da2c82ee66890dd20d54ed5a218e7717f982514d
Max Tyler <maxastyler@gmail.com>
2020-02-01 23:49:36 +0000

    Make some changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -32,7 +32,7 @@
  '(custom-safe-themes
    '("6973f93f55e4a6ef99aa34e10cd476bc59e2f0c192b46ec00032fe5771afd9ad" "179ff455fbab61b1c5be8da791c53c4a2b65598dc372031be1e95373bd9a1f25" default))
  '(package-selected-packages
-   '(lsp-mode treemacs-projectile treemacs poetry nix-mode poet-theme lispy yasnippet-snippets winum which-key undo-tree auctex rustic ripgrep rainbow-delimiters projectile org-ref org-bullets multi-term magit helm-swoop helm-bibtex helm flycheck eyebrowse async elpy dracula-theme doom-modeline company cider use-package)))
+   '(lsp-ivy lsp-mode treemacs-projectile treemacs poetry nix-mode poet-theme lispy yasnippet-snippets winum which-key undo-tree auctex rustic ripgrep rainbow-delimiters projectile org-ref org-bullets multi-term magit helm-swoop helm-bibtex helm flycheck eyebrowse async elpy dracula-theme doom-modeline company cider use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
