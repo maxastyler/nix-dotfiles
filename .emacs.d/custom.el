b17d43e81aad4e4de45fdd5ab49f23dbe681822f
Max Tyler <maxastyler@gmail.com>
2020-04-21 17:31:30 +0100

    Add custom file

---
+++ .emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (rustic yasnippet-snippets which-key use-package undo-tree treemacs-projectile slime-company ripgrep rainbow-delimiters org-ref org-bullets ob-ipython nix-mode multi-term magit lsp-ui lispy hy-mode helm-swoop helm-lsp flycheck eyebrowse elpy dracula-theme doom-modeline direnv dap-mode company-lsp cider cargo auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
