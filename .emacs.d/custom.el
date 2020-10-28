053fb3d52bc550ef007dd8487faed4c63e84e94c
Max Tyler <maxastyler@gmail.com>
2020-10-28 16:24:28 +0000

    Add new thoughts org file

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yasnippet-snippets which-key use-package undo-tree treemacs-projectile sphinx-doc slime-company rustic ripgrep rainbow-delimiters org-ref org-bullets ob-ipython nix-mode multi-term magit lsp-ui lispy julia-repl julia-mode hy-mode helm-swoop helm-lsp flycheck eyebrowse elpy dracula-theme doom-modeline direnv dap-mode company-lsp cider cargo auctex))))
+    (yasnippet-snippets writeroom-mode which-key vterm use-package treemacs-projectile sphinx-doc slime-company rustic ripgrep rainbow-delimiters org-ref org-bullets ob-ipython nix-mode multi-term magit lsp-ui lispy julia-repl julia-mode hy-mode helm-swoop helm-lsp flycheck eyebrowse evil elpy edit-indirect dracula-theme doom-modeline direnv dap-mode company-lsp cider cargo auctex)))
+ '(safe-local-variable-values
+   (quote
+    ((cider-shadow-cljs-default-options . "app")
+     (cider-default-cljs-repl . shadow)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
