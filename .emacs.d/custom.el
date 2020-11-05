fea1f1a85f7688f19b7b0e01e07ab582aa449e2d
Max Tyler <maxastyler@gmail.com>
2020-11-05 11:23:39 +0000

    Add compilation window scrolling and mix mode

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,11 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yasnippet-snippets writeroom-mode which-key vterm use-package treemacs-projectile sphinx-doc slime-company rustic ripgrep rainbow-delimiters org-ref org-bullets ob-ipython nix-mode multi-term magit lsp-ui lispy julia-repl julia-mode hy-mode helm-swoop helm-lsp flycheck eyebrowse evil elpy edit-indirect dracula-theme doom-modeline direnv dap-mode company-lsp cider cargo auctex)))
- '(safe-local-variable-values
-   (quote
-    ((cider-shadow-cljs-default-options . "app")
-     (cider-default-cljs-repl . shadow)))))
+    (mix yasnippet-snippets writeroom-mode which-key vterm use-package treemacs-projectile sphinx-doc slime-company rustic ripgrep rainbow-delimiters org-ref org-bullets ob-ipython nix-mode multi-term magit lsp-ui lispy julia-repl julia-mode hy-mode helm-swoop helm-lsp gdscript-mode flycheck eyebrowse evil elpy elixir-mode edit-indirect dracula-theme doom-modeline direnv dap-mode company-lsp cider cargo auctex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
